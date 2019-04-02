65d175d9615d2d03157d5454ee264e25ab3e3d0f
sunzhengyu <15927522625@163.com>
2019-04-02 19:37:03 +0800

    dev: 统计显示各个分类下的文章数    基于django annotate 以及聚合 实现

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Filename:XX.py
+from django.db.models.aggregates import Count
 from django import template
 from ..models import Post, Category
 
@@ -22,4 +23,5 @@ def archives():
 # 分类模板标签
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    # 获取分类时 统计分类下的文章数 并过滤文章数为0的分类 return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
